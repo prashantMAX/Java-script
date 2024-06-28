d463937250980dfcd3285feee17b3920f7797b40
prashantMAX <118283008+prashantMAX@users.noreply.github.com>
2024-06-28 20:24:57 +0200

    xcv
    xcvxcv

--- ALGORITHME/chapters/arithmetic/student.js
+++ ALGORITHME/chapters/arithmetic/student.js
@@ -6,78 +6,83 @@ return 5
 function average(){
     return (135+471)/2
 }
+
+
 function average2(x,y){
     return (x+y)/2
 }
+
 function average3(x,y,z){
+
     return (x+y+z)/3
 }
 
 function distance(x,y){
 
-return(x**2+y**2)**(1/2)
+    return (x**2 + y**2)**0.5
 }
 
-function busesNeeded(people,busCapacity){
 
-    return Math.ceil(people/busCapacity)
+function busesNeeded(people, busCapacity){
+
+    
+    return  Math.ceil(people/busCapacity) 
 }
+
 function sugar(pies){
 
     return Math.ceil(pies/4)
 }
+
 function candyPerChild(children,candy){
-    return Math.floor(candy/children)
-}
-function  leftoverCandy(children, candy){
 
-    return candy%children
+  return  Math.floor(candy/children)
 }
 
-function hours(totalminutes){
+function leftoverCandy(children,candy){
 
-    return Math.floor(totalminutes/60)
+    return candy %children
 }
-function minutes(totalminutes){
-    return totalminutes%60
+
+function hours(totalMinutes){
+
+    return Math.floor(totalMinutes/60)
 }
+
+function minutes(totalMinutes){
+    return totalMinutes % 60
+}
+
 function communicationCosts(seconds){
-return Math.ceil(seconds/60)*0.20
-}
-function distance2(x1,y1,x2,y2){
-   let dx = x2-x1 
-   let dy =  y2-y1 
-
-   return (dx**2+dy**2)**0.5
+   return  0.2 * Math.ceil(seconds/60)
 }
 
-function  cake(eggs){
+function distance2(a,y,b,z){
+
+    return ((b-a)**2 + (z-y)**2)**0.5
+}
+
+function cake(eggs){
 
     return Math.floor(eggs/3)
 }
+
 function cake2(eggs,flour){
-     let neededeggs = Math.floor(eggs/4)
-     let neededflour =  Math.floor(flour/250)
-    return Math.min(neededeggs,neededflour)
+
+    return Math.min(Math.floor(eggs/4),Math.floor(flour/250))
 }
+
 
 function cake3(eggs,flour,sugar,butter){
-    let neededeggs = Math.floor(eggs/4)
-     let neededflour =  Math.floor(flour/250)
-     let neededsugar =  Math.floor(sugar/250)
-     let neededbutter =  Math.floor(butter/250)
 
-     return Math.min(neededsugar,neededbutter,neededflour,neededeggs)
-
+    return Math.min(Math.floor(eggs/4),Math.floor(flour/250),Math.floor(sugar/250),Math.floor(butter/250))
 }
 
-function roundToMultiple(n,multipleof){
-    return Math.round(n / multipleof) * multipleof
+function roundToMultiple(n,multipleOf){
+    return Math.round(n/multipleOf)*multipleOf
 }
 
 function middle(a,b,c){
-    let grootste = Math.max(a,b,c)
-    let kleinste = Math.min(a,b,c)
-    return a+b+c- (grootste+kleinste)
+    var som = a+b+c
+    return som - Math.min(a,b,c) - Math.max(a,b,c) 
 }
-
--- ALGORITHME/chapters/booleans/student.js
+++ ALGORITHME/chapters/booleans/student.js
@@ -1,77 +1,40 @@
-// Schrijf hier je code
+
 function isPositive(x){
-  
-   return x>=0
+   
+    return x>=0
 }
+
 function isNegative(x){
-    return x<=0
 
+    return x<=0
 }
 
-function isDivisibleBy(x,y) {
-     
-    return 0===x%y
+function isDivisibleBy(x,n){
+
+    return x%n===0
 }
 
 function inRange(x,min,max){
-    return min<=x  &&  x<=max
+
+    return x>=min && x<=max
 }
 
-function pingPong(x,y){
-    return x>=11 && x>=y+2
+function pingPong(score1,score2){
+
+    return score1>=11 && score1>=2+score2
 }
+
 function freeTicket(age){
 
-
-    return age>=65 || age<12
+    return age<12 || age>=65
 }
 
-function has30Days(month) {
-    return month === 4 || month === 6 || month === 9 || month === 11;
-}
-function has31Days(month) {
-    return month === 1 || month === 3 || month === 5 || month === 7 || month == 8 || month == 10 || month == 12;
-}
-function isLeapYear(year){
-return year%4 === 0 && year%100!==0 || year % 400 ===0 ;
+function has30Days(month){
+
+    return [4,6,9,11].includes(month)
 }
 
-function has28Days(month,year){
+function has31Days(month){
 
-    return !isLeapYear(year) && month===2
+    return ![2,4,6,9,11].includes(month)
 }
-function has29Days(month,year){
-    return isLeapYear(year) && month===2 && !has28Days(month,year)
-}
-
-function isValidDate(day, month, year) {
-    return inRange(month, 1, 12) &&
-        ((has31Days(month) && inRange(day, 1, 31)) ||
-            (has30Days(month) && inRange(day, 1, 30)) ||
-            (has28Days(month, year) && inRange(day, 1, 28)) ||
-            (has29Days(month, year) && inRange(day, 1, 29)));
-}
-function majority(x,y,z){
-    return (x===true && y===true&& z===true)|| (x===false && y===true&& z===true)||(x===true && y===false&& z===true)||(x===true && y===true&& z===false)
-}
-
-function isHigherCard(x,y){
-
-
-    return    (x>y || x===1) && y!=1 
-}
-function isInteger(x){
-
-return x%1===0
-}
-
-function isPerfectSquare(x){
-    return isInteger(x**0.5)
-}
-
-function earlier(a,b,c,d)
-{
-
-return  (a===c && b<d) || a<c
-}
-
